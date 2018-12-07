372d8a05fec118024c8578c11d44492636773cdf
Zeswen <zeswentyrael@gmail.com>
2018-12-07 15:44:41 +0100

    Iteration 1

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,14 +2,13 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
-const parser = require("../config/cloudinary")
+const parser = require("../config/cloudinary");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 5;
 
 router.post("/signup", (req, res) => {
-  console.log(req.body)
   const { username, password, campus, course } = req.body;
   if (username === "" || password === "" || campus === "" || course === "") {
     res
@@ -23,8 +22,6 @@ router.post("/signup", (req, res) => {
       res.status(500).json({ message: "The username already exists" });
       return;
     }
-
-    console.log(password)
 
     const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(bcryptSalt));
 
@@ -47,12 +44,18 @@ router.post("/signup", (req, res) => {
   });
 });
 
-router.post('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err); }
-    if (!user) { return res.json({message: "Error on login"}); }
+router.post("/login", function(req, res, next) {
+  passport.authenticate("local", function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.json({ message: "Error on login" });
+    }
     req.logIn(user, function(err) {
-      if (err) { return next(err); }
+      if (err) {
+        return next(err);
+      }
       return res.json(user);
     });
   })(req, res, next);
@@ -63,6 +66,31 @@ router.post("/upload", parser.single("picture"), (req, res) => {
     res.json({
       success: true,
       pictureUrl: req.file.url
+    });
+  });
+});
+
+router.post("/edit", (req, res) => {
+  const { username, password, campus, course } = req.body;
+
+  const myUser = {};
+
+  if (username) {
+    myUser.username = username;
+  }
+
+  if (campus) {
+    myUser.campus = campus;
+  }
+
+  if (course) {
+    myUser.course = course;
+  }
+
+  User.findByIdAndUpdate(req.user.id, myUser).then(() => {
+    res.json({
+      success: true,
+      myUser
     });
   });
 });
